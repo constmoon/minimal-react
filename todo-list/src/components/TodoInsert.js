5aa110de1142466bfda6d4b41053ea20ced1d707
Jiwoo Kang <jiiw.kang@gmail.com>
2019-12-30 18:08:38 +0900

    feat(TodoInsert): 할 일 추가 기능 구현

--- todo-list/src/components/TodoInsert.js
+++ todo-list/src/components/TodoInsert.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [ value, setValue ] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+  
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  )
   return (
-    <form className="todo-form">
-      <input placeholder="TODO" />
+    <form className="todo-form" onSubmit={onSubmit}>
+      <input
+        placeholder="TODO"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
