0c0dd401a8470fdbc6e545ff9c4625d9960c0174
constmoon <jiiw.kang@gmail.com>
2020-03-28 23:40:46 +0900

    feat: React 없이 Redux 사용

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 // DOM refrence
 const toggle = document.querySelector('.toggle');
 const counter = document.querySelector('.num');
@@ -18,4 +20,56 @@ const decrease = () => ({ type: DECREASE });
 const initialState = {
   toggle: false,
   counter: 0
+}
+
+// Reducer
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+}
+
+// Store
+const store = createStore(reducer);
+
+// Render
+const render = () => {
+  const state = store.getState();
+  
+  if(state.toggle) {
+    toggle.classList.add('active');
+  } else {
+    toggle.classList.remove('active');
+  }
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+// Action
+toggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+}
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
 }
